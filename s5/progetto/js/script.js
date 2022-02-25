0f097c7cd6d785f3d3e20e81745a645676c611ed
camizmb <camizmb@gmail.com>
2022-02-25 13:52:00 +0100

    progetto

---
+++ s5/progetto/js/script.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+document.querySelector('#add').addEventListener('click', function () {
+    let valore = document.querySelector('#testo').value;
+
+    if(!valore)
+    return
+   
+
+    let li = document.createElement('li')
+    li.innerText = valore
+
+    document.querySelector('#lista').append(li)
+    document.querySelector('#testo').value = ''
+
+    li.addEventListener('click', function () {
+        li.remove()
+        elimina()
+
+    })
+    salva(valore)
+})
+
+function salva(testo) {
+    let elementiSalvati = localStorage.getItem('lista')
+    let db = elementiSalvati == null ? [] : JSON.parse(elementiSalvati)
+    db.push(testo)
+    localStorage.setItem('lista', JSON.stringify(db))
+
+}
+
+function creaHTML() {
+    let elementiSalvati = localStorage.getItem('lista')
+    let db = elementiSalvati == null ? [] : JSON.parse(elementiSalvati)
+
+    db.forEach(function (elemento) {
+        let li = document.createElement('li')
+        li.innerText = elemento
+        document.querySelector('#lista').append(li)
+        document.querySelector('#testo').value = ''
+
+        li.addEventListener('click', function () {
+            li.remove()
+            elimina(elemento)
+        })
+        salva(elemento)
+    })
+
+}
+
+creaHTML()
+
+function elimina(testo) {
+    let elementiSalvati = localStorage.getItem('lista')
+    let db = elementiSalvati == null ? [] : JSON.parse(elementiSalvati)
+    let indice = db.indexOf(elemento)
+    db.splice(indice, 1)
+    localStorage.setItem('lista', JSON.stringify(db))
+
+
+}
